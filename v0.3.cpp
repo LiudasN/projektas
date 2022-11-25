b158259ce012e6e3497537dbc0e3d6fd1784c795
LiudasN <114232083+LiudasN@users.noreply.github.com>
2022-11-25 10:26:42 +0200

    Update v0.3.cpp

--- v0.3.cpp
+++ v0.3.cpp
@@ -94,17 +94,3 @@ int main() {
         cout << "Atlikta. Laikas: " << std::chrono::duration <double, milli>(ending_main - start_main).count() << " ms" << endl;
     }
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
